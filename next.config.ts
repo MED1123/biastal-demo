f6c605e0675f3487178695b07279bd31dbf54fd6
Norbert Pietrzak <norbertpietrzak@MacBook-Pro-Norbert.local>
2026-02-26 22:16:21 +0100

    zmiany

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,13 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig: NextConfig = {
   basePath: isProd ? '/pl' : '',
   trailingSlash: false,
+  async redirects() {
+    // Na produkcji przekieruj root / → /pl (bo aplikacja jest pod basePath)
+    return isProd
+      ? [{ source: '/', destination: '/pl', permanent: false, basePath: false }]
+      : [];
+  },
 };
 
 export default nextConfig;
+
